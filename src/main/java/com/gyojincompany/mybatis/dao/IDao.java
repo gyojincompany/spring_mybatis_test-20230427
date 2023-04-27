8212d9dad5720441c3ae1180e335455ef9e03afd
ICI <ICI@DESKTOP-EA5M9P6>
2023-04-27 14:11:54 +0900

    글 삭제 기능 추가

--- src/main/java/com/gyojincompany/mybatis/dao/IDao.java
+++ src/main/java/com/gyojincompany/mybatis/dao/IDao.java
@@ -8,5 +8,6 @@ public interface IDao {
 	
 	public void writeDao(String mwriter, String mcontent);//게시판 글쓰기
 	public ArrayList<BoardDto> listDao();//게시판 리스트 가져오기
+	public void deleteDao(String mid);//게시글 삭제
 
 }
